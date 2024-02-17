745198ef0e9c26b7665957fff2702a04906b39b2
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-02-17 23:44:52 +0300

    feat: add manager user role view

--- littlelemonAPI/views.py
+++ littlelemonAPI/views.py
@@ -173,3 +173,12 @@ def menu_items_yaml(request):
 @permission_classes([IsAuthenticated])
 def secret(request):
     return Response({"message": "Some secret message"})
+
+# Mnaager user Role - Authentication
+@api_view()
+@permission_classes([IsAuthenticated])
+def manager_view(request):
+    if request.user.groups.filter(name='Manager').exists():
+        return Response({"message": "Only Manager should see this"})
+    else:
+        return Response({"message": "You are not authorized"}, 403)
