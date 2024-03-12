528c01832e49dc5cd1b6c6a4929d8f4236618ba2
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-03-12 10:54:07 +0300

    feat: add urls to simplejwt

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,8 @@ urlpatterns = [
     # Djoser endpoints
     path("auth/", include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    # JWT
+    path("api/token/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
