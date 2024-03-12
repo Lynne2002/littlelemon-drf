b2beda0109532c94724b0d9d71cdeab10585ebbc
Lynne Chepkwony <lynnebebet2@gmail.com>
2024-03-12 10:54:29 +0300

    feat: add simplejwt to installed apps

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'littlelemonAPI',
     'debug_toolbar',
     'djoser',
+    'rest_framework_simplejwt'
 
 ]
 
@@ -155,6 +157,7 @@ REST_FRAMEWORK ={
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'DEFAULT_THROTTLE_RATES':{
         'anon': '2/minute',
@@ -165,4 +168,8 @@ REST_FRAMEWORK ={
 DJOSER = {
     "USER_ID_FIELD": "username",
     #"LOGIN_FIELD": "email" for using email instead
+}
+# Specifying expiry of JWT access tokens
+SIMPLE_JWT ={
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5)
 }
